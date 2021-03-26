f5a9df86d7bb0b19dc92330fdc20ca82fd9dd37a
Luthfi <mluthfi30@gmail.com>
2021-03-26 08:17:21 +0700

    Second Commit, 25/03

---
+++ GeometryAR/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  GeometryAR
+//
+//  Created by Luthfi on 25/03/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .darkContent
+    }
+    
+    override func viewDidLoad() {
+    
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GeometryAR/ViewController.swift
+++ GeometryAR/ViewController.swift
@@ -13,28 +13,55 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    
+    var coneNode, cubeNode, cuboidNode, pyramidNode, cylinderNode, sphereNode, tPyramidNode, tPrismNode : SCNNode?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
+    
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+       
+        let cubeScene = SCNScene(named: "art.scnassets/box.scn")
+        let cuboidScene = SCNScene(named: "art.scnassets/cuboid.scn")
+        let pyramidScene = SCNScene(named: "art.scnassets/pyramid.scn")
+        let cylinderScene = SCNScene(named: "art.scnassets/cylinder.scn")
+        let coneScene = SCNScene(named: "art.scnassets/cone.scn")
+        let sphereScene = SCNScene(named: "art.scnassets/sphere.scn")
+        let tPyramidScene = SCNScene(named: "art.scnassets/t_pyramid.scn")
+        let tPrismScene = SCNScene(named: "art.scnassets/t_prism.scn")
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        cubeNode = cubeScene?.rootNode
+        cuboidNode = cuboidScene?.rootNode
+        pyramidNode = pyramidScene?.rootNode
+        cylinderNode = cylinderScene?.rootNode
+        coneNode = coneScene?.rootNode
+        sphereNode = sphereScene?.rootNode
+        tPyramidNode = tPyramidScene?.rootNode
+        tPrismNode = tPrismScene?.rootNode
         
-        // Set the scene to the view
-        sceneView.scene = scene
+       
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
 
+        
+        // Get markerImages
+        
+        if let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "markers", bundle: Bundle.main) {
+            configuration.trackingImages = trackingImages
+            configuration.maximumNumberOfTrackedImages = 8
+        }
+        
+        
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -45,30 +72,79 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            
+            let size = imageAnchor.referenceImage.physicalSize
+            let plane = SCNPlane(width: size.width, height: size.height)
+            plane.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
+            plane.cornerRadius = 0.005
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
+            node.addChildNode(planeNode)
+            
+            print(imageAnchor.referenceImage.name ?? "unknown")
+            
+            var shapeNode: SCNNode?
+            
+            switch imageAnchor.referenceImage.name {
+            case CardType.cube.rawValue:
+                shapeNode = cubeNode
+            case CardType.cuboid.rawValue:
+                shapeNode = cuboidNode
+            case CardType.cone.rawValue:
+                shapeNode = coneNode
+            case CardType.cylinder.rawValue:
+                shapeNode = cylinderNode
+            case CardType.square_pyramid.rawValue:
+                shapeNode = pyramidNode
+            case CardType.sphere.rawValue:
+                shapeNode = sphereNode
+            case CardType.tPyramid.rawValue:
+                shapeNode = tPyramidNode
+            case CardType.tPrism.rawValue:
+                shapeNode = tPrismNode
+            default:
+                break
+            }
+            
+            let shapeSpin = SCNAction.rotateBy(x: 0, y: 2 * .pi, z: 0, duration: 10)
+            let repeatSpin = SCNAction.repeatForever(shapeSpin)
+            shapeNode?.runAction(repeatSpin)
+
+            guard let shape = shapeNode else {return nil}
+            node.addChildNode(shape)
+//
+            
+        }
+        
+        return node
+        
+    }
+    
+    enum CardType : String {
+        
+        case cube = "cube"
+        case cuboid = "cuboid"
+        case cone = "cone"
+        case cylinder = "cylinder"
+        case sphere = "sphere"
+        case square_pyramid = "square-pyramid"
+        case tPyramid = "triangular-pyramid"
+        case tPrism = "triangular-prism"
+        
+    }
+    
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
+
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
